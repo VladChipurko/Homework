180533a366371fed1ced72495e33fb41c84ef644
Vlad Chipurko <vladchipurko@gmail.com>
2022-01-13 20:26:27 +0200

    Доработал компаратор Объяснил как сортировать с null-ами

--- home_work_4/service/comparators/IntegerComparator.java
+++ home_work_4/service/comparators/IntegerComparator.java
@@ -3,10 +3,20 @@ package home_work_4.service.comparators;
 import home_work_4.dto.DataContainer;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class IntegerComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
+        if(Objects.equals(o1, o2)){
+            return 0;
+        }
+        if(o1 == null){
+            return -1;
+        }
+        if(o2 == null){
+            return 1;
+        }
         return o1 - o2;
     }
 }
--- home_work_4/service/comparators/StringComparator.java
+++ home_work_4/service/comparators/StringComparator.java
@@ -3,10 +3,20 @@ package home_work_4.service.comparators;
 import home_work_4.dto.DataContainer;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class StringComparator implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
+        if(Objects.equals(o1, o2)){
+            return 0;
+        }
+        if(o1 == null){
+            return -1;
+        }
+        if(o2 == null){
+            return 1;
+        }
         return o1.compareTo(o2);
     }
 }
